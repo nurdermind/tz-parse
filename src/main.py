a74c1589d25ef1d383cdbca18dae6eb146f04ecd
nerdermind <mansur.tumashev@gmail.com>
2024-02-03 20:52:34 +0500

    started integrate redis queue

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
+from redis import Redis
+from rq import Queue
+from rq.job import Job
 
 from core.parser import Parser
+
+redis_conn = Redis(host='127.0.0.1', port=6379)
+q = Queue(connection=redis_conn)
 
 app = FastAPI()
 
@@ -10,8 +16,11 @@ app = FastAPI()
 async def json_endpoint(request: Request):
     body = await request.body()
     raw_json = body.decode("utf-8")
-    parsed_data = Parser([raw_json]).parse()
-    return JSONResponse(content={"received_data": parsed_data})
+    parsed_data = Parser([raw_json])
+    job = Job.create(Parser([raw_json]).parse)
+    job = q.enqueue_job(job)
+
+    return JSONResponse(content={"job_id": job.id})
 
 
 @app.post("/xml-endpoint/")
